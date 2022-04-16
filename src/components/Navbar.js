3ee31f9dfba7f382b035cfc7577f586d9c6dca73
Shubham Kumar Maurya <skmaurya9721@gmail.com>
2022-04-16 21:43:47 +0530

    home remove

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,9 +22,7 @@ import React from "react";
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
+               
               </li>
                 
             </ul>
